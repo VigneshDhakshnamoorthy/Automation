8d861b64b81d29179f5b2d853e227ca09c10dccb
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-03-16 11:44:50 +0530

    file edited

--- JavaTestInterview/src/javaTest/ListDuplicateFinder.java
+++ JavaTestInterview/src/javaTest/ListDuplicateFinder.java
@@ -2,25 +2,38 @@ package javaTest;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ListDuplicateFinder {
 	
 	public static void duplistfind(List<String> names) {
 		Set<String> nameset = new HashSet<String>(); 
+		Map<String, Integer> namemap = new HashMap<String, Integer>();
 		
 		for (String name : names) {
 			nameset.add(name);
-		
+			if(namemap.containsKey(name))
+			{
+				namemap.put(name, namemap.get(name)+1);
+			}else {
+				namemap.put(name, 1);
+			}
+					
 		}
+		System.out.println(namemap);
+
+		
 		for (String named : nameset) {
 			
 			System.out.println(named+": "+Collections.frequency(names, named));
 			
 		}
-
+		
+		
 	}
 
    
@@ -31,6 +44,7 @@ public class ListDuplicateFinder {
     	names.add("Vicky");
     	names.add("Vignesh");
         ListDuplicateFinder.duplistfind(names);
+        System.out.println();
         
         		
       
