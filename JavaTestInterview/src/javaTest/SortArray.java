1e573e3a4bbb836149c9658270fe23febac9a70e
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-03-17 17:32:14 +0530

    sort_array

--- JavaTestInterview/src/javaTest/SortArray.java
+++ JavaTestInterview/src/javaTest/SortArray.java
@@ -14,7 +14,7 @@ public class SortArray {
 	    }
 	public static void main(String[] args) {
 		
-		int[] arr = new int[] {78, 34, 1, 3, 90, 34, -1, -4, 6, 55, -20, -65};  
+		int[] arr = new int[] {78, 34, 1, 3, 90, 34, -4, 6, 55, -20, -65};  
 		int[] newArr = new int[arr.length];
 		System.out.println("Array elements after sorting:");  
 		//sorting logic  
