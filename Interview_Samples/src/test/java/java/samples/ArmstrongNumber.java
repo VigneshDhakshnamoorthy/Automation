ce07b5f8ed7dda19e759285f6eb02d076cc3f211
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-03-20 15:28:49 +0530

    new project added

---
+++ Interview_Samples/src/test/java/java/samples/ArmstrongNumber.java
@@ -0,0 +1,29 @@
+package java.samples;
+
+public class ArmstrongNumber {
+	
+	public static void ArmStro(int a) {
+		
+		double sum =0;
+		String num = Integer.toString(a);
+		char[] numc = num.toCharArray();
+		for ( char numca : numc) {
+			int numci = Character.getNumericValue(numca);
+			sum = sum + (Math.pow(numci, num.length()));
+		}
+		if ( a == sum ) {
+			System.out.println(a+" is Armstrong Number");
+			
+		} 
+		
+	}
+	
+	public static void main(String[] args) {
+		for ( int i = 10 ; i<10000 ; i++)
+		{
+		ArmstrongNumber.ArmStro(i);
+			}
+		}
+	
+
+}
---
+++ Interview_Samples/src/test/java/java/samples/Fibonacci.java
@@ -0,0 +1,34 @@
+package java.samples;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void fibon() {
+		
+		int c = 0;
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter First Number : ");
+		int a = scan.nextInt();
+		System.out.println("Enter Second Number : ");
+		int b = scan.nextInt();
+		System.out.println("Enter Rows Number : ");
+		int row = scan.nextInt();
+	
+
+		for (int i=0; i<row; i++) {
+			System.out.println(a);
+			c = a+b;
+			a = b;			
+			b = c;
+			
+		}
+
+
+	}
+
+	public static void main(String[] args) {
+		Fibonacci.fibon();
+	}
+}
