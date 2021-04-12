88641abf4ae69bfd0f9356d71f127d22bd9dac7c
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-12 17:55:12 +0530

    java version changed

--- DX_tester/src/test/java/stepDefinations/StepDefination.java
+++ DX_tester/src/test/java/stepDefinations/StepDefination.java
@@ -18,28 +18,28 @@ public class StepDefination extends BrowserSetup {
 		
 	}
 
-	@And("Need to verify the right count of values appear on the screen")
+	@Given("Need to verify the right count of values appear on the screen")
 	public void need_to_verify_the_right_count_of_values_appear_on_the_screen() {
 		Main = PageFactory.initElements(driver, MainPage.class);
 		Main.ValuesNames();
 	}
 
-	@And("Need to verify the values on the screen are greater than {int}")
+	@Given("Need to verify the values on the screen are greater than {int}")
 	public void need_to_verify_the_values_on_the_screen_are_greater_than(Integer int1) {
 		Main.ValueAmounts();
 	}
 
-	@And("Need to verify the total balance is correct based on the values listed on the screen")
+	@Given("Need to verify the total balance is correct based on the values listed on the screen")
 	public void need_to_verify_the_total_balance_is_correct_based_on_the_values_listed_on_the_screen() {
 		Main.TotalBal();
 	}
 
-	@And("Need to verify the values are formatted as currencies")
+	@Given("Need to verify the values are formatted as currencies")
 	public void need_to_verify_the_values_are_formatted_as_currencies() {
 		Main.Currency();
 	}
 
-	@And("Need to verify the total balance matches the sum of the values")
+	@Given("Need to verify the total balance matches the sum of the values")
 	public void need_to_verify_the_total_balance_matches_the_sum_of_the_values() {
 		Main.Verify_TotalBal_Values();
 	}
