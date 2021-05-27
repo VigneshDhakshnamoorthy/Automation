8abd933b0b6ddc9b20c635a15395a4b5332ad695
Vignesh <vigneshdhakshnamoorthy@gmail.com>
2021-05-27 14:44:16 +0530

    excelread

--- InterviewSample/src/test/java/interview/sample/excel_handle.java
+++ InterviewSample/src/test/java/interview/sample/excel_handle.java
@@ -1,5 +1,7 @@
 package interview.sample;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,6 +20,7 @@ public class excel_handle {
 	
 	public static void main(String[] args) throws Throwable {
 		
+        String File_loc = "./src/test/java/excel/rediff.xlsx";
 		WebDriver driver;
 		FirefoxOptions firefoxOptions = new FirefoxOptions();
 		firefoxOptions.addArguments("--headless");
@@ -25,12 +28,7 @@ public class excel_handle {
 		driver = new FirefoxDriver(firefoxOptions);
 		driver.get("https://money.rediff.com/gainers/bse/daily/groupz");
 		List<WebElement> table1  = driver.findElements(By.xpath("//table[@class=\"dataTable\"]/tbody/tr/td/a"));
-		
-		for(WebElement tab:table1) {
-			
-			System.out.println(tab.getText());
-		}
-		
+
 		XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("rediff");
         int count = 0;
@@ -39,12 +37,27 @@ public class excel_handle {
         	   XSSFCell cell = row.createCell(0);
         	   cell.setCellValue(tab.getText());
         	   count++;
-        			
+        	
         	
         }
         driver.quit();
-        FileOutputStream rediff_excel = new FileOutputStream("./src/test/java/excel/rediff.xlsx");
+        FileOutputStream rediff_excel = new FileOutputStream(File_loc);
 		workbook.write(rediff_excel);
+		
+		File read_ex = new File(File_loc);
+		FileInputStream read_exc = new FileInputStream(read_ex);
+		XSSFWorkbook read_book = new XSSFWorkbook(read_exc);
+		XSSFSheet read_sheet = read_book.getSheet("rediff");
+		int Row_count = read_sheet.getLastRowNum()-read_sheet.getFirstRowNum();
+		for ( int i =0; i<Row_count;i++) {
+			XSSFRow read_row = read_sheet.getRow(i);
+			XSSFCell read_cell = read_row.getCell(0);
+			System.out.println(read_cell);
+		}
+		
+		
+		
+		
 	}
 
 }
